ea6c8d8343ee500f0e25793ac37a173f4a903433
Sergey Samohvalov <Samohvalov@tlpt.ru>
2019-06-23 19:05:06 +0300

    Add interval fitch - new data

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const http = require('http')
-const port = 4000
+const http = require('http'),
+  port = 4000;
 
 const requestHandler = (request, response) => {
-    console.log(request.url)
-    response.end('Hello node.js server')
-}
+    let = interval = process.argv[2],
+    timeout = process.argv[3];
 
-const server = http.createServer(requestHandler)
+  const inter = setInterval(() => {
+    console.info(`Вывод текущей даты и времени - ${new Date().toUTCString()}`);
+  }, interval);
 
-server.listen(port, (err) => {
-    err ? console.log(err) : console.log(`Server is listening on ${port}`)
-})
+  setTimeout(() => {
+    clearInterval(inter);
+    console.info(
+      `Очистка интервала - через - ${timeout}, Время остановки -  ${new Date().toUTCString()}`
+    );
+  }, timeout);
+
+  response.end('Hello node.js server');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, err => {
+  err ? console.log(err) : console.log(`Server is listening on ${port}`);
+});
