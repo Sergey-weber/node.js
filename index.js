11ef1df7adc59b1dfa4c3d9ce3130478b187dc4c
Sergey Samohvalov <Samohvalov@tlpt.ru>
2019-06-24 21:38:00 +0300

    Fix interval

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const http = require('http'),
-  port = 4000;
+const http = require('http')
+const port = 4000;
+  
+let inter = undefined
 
 const requestHandler = (request, response) => {
     let = interval = process.argv[2],
     timeout = process.argv[3],
     sumInterval = 0
+  
+  inter ? clearInterval(inter) : undefined
 
-  const inter = setInterval(() => {
+  inter = setInterval(() => {
     console.info(`Вывод текущей даты и времени - ${new Date().toUTCString()}`);
     sumInterval += parseInt(interval)
     if(sumInterval > timeout) {
       clearInterval(inter);
-      
+    
       console.info(
-        `Очистка интервала - через - ${timeout}, Время остановки -  ${new Date().toUTCString()}`)
+      `Очистка интервала - через - ${timeout}, Время остановки -  ${new Date().toUTCString()}`)
     }
-  }, interval);
-
+  }, interval)
 
   response.end(new Date().toUTCString());
 };
