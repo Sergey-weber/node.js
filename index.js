9f621f1c2e8edf7cc9e6ee6ccf0154edb41bde70
Sergey Samohvalov <Samohvalov@tlpt.ru>
2019-06-24 19:52:57 +0300

    Fix timeout

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const http = require('http'),
 
 const requestHandler = (request, response) => {
     let = interval = process.argv[2],
-    timeout = process.argv[3];
+    timeout = process.argv[3],
+    sumInterval = 0
 
   const inter = setInterval(() => {
     console.info(`Вывод текущей даты и времени - ${new Date().toUTCString()}`);
+    sumInterval += parseInt(interval)
+    if(sumInterval > timeout) {
+      clearInterval(inter);
+      
+      console.info(
+        `Очистка интервала - через - ${timeout}, Время остановки -  ${new Date().toUTCString()}`)
+    }
   }, interval);
 
-  setTimeout(() => {
-    clearInterval(inter);
-    console.info(
-      `Очистка интервала - через - ${timeout}, Время остановки -  ${new Date().toUTCString()}`
-    );
-  }, timeout);
 
-  response.end('Hello node.js server');
+  response.end(new Date().toUTCString());
 };
 
 const server = http.createServer(requestHandler);
